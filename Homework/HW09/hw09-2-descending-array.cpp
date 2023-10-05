4d25213c8d3bfe7d1c7cb3758361d2c927074be2
Ploy Primchayaporn <137378257+Primchayaporn@users.noreply.github.com>
2023-10-05 13:40:28 +0700

    hw9-2

--- Homework/HW09/hw09-2-descending-array.cpp
+++ Homework/HW09/hw09-2-descending-array.cpp
@@ -23,3 +23,62 @@
         99-0 : 13 9 6 5 4 1 1
 
 */
+#include <stdio.h>
+
+int main() {
+    int n = 100 ;
+    int a[n] ;  
+    int c = 1 , i = 0 , j = 0 , min = 0 , max = 0 ; 
+    for (int i = 0 ; i < n ; i++ ) {
+        printf( "Input :\n" );
+        scanf( "%d", &a[i] ) ;
+        if ( a[ i ] < 0 ) {
+            break ;
+        }
+        c++ ;
+    } //end for loop
+
+    // printf( "array = " ) ;
+    // for (int i = 0 ; i < c-1 ; i++) {
+    //     printf("%d ", a[i]);
+
+    // }//end for array
+
+    
+    // printf( "\n" ) ;
+    // printf( "c= %d", c ) ;
+    // printf( "\n" ) ;
+    
+    printf( "0-99 :" ) ;
+    for (i = 0 ; i < c-1 ; i++) {
+        for ( j = i + 1 ; j < c - 1 ; j++ ) {
+            if ( a [ i ] > a[ j ] ) {
+                min = a[ j ] ;
+                a[ j ] = a [ i ] ;
+                a[ i ] = min ;
+            }
+        }
+    }
+    for ( i = 0 ; i < c - 1 ; i++) {
+        printf(" %d", a[i]);
+    }
+
+    //end 0-99
+
+    printf( "\n" ) ;
+    printf( "99-0 :" ) ;
+    for (i = 0 ; i < c-1 ; i++) {
+        for ( j = i + 1 ; j < c - 1 ; j++ ) {
+            if ( a [ j ] > a[ i ] ) {
+                min = a[ i ] ;
+                a[ i ] = a [ j ] ;
+                a[ j ] = min ;
+            }
+        }
+    }
+    for ( i = 0 ; i < c - 1 ; i++) {
+        printf(" %d", a[i]);
+    }
+
+    return 0;
+}
